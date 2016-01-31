29ad037ad69c857c9512291be2d206522843b87a
romkatv <roman.perepelitsa@gmail.com>
2016-01-31 13:45:02 +0100

    Generic scheduler for running actions asynchronously and serially.

---
+++ ExchangeApi/Infra/ScheduledQueue.cs
@@ -0,0 +1,83 @@
+﻿using Conditions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExchangeApi
+{
+    class ReadyMessage<T>
+    {
+        // The payload.
+        public T Message;
+        // If true, there are no ready messages behind this one.
+        // In other words, this is the last ready message.
+        public bool Last;
+    }
+
+    // A queue of items with their expected processing time.
+    // Thread-safe for multiple producers and single consumer.
+    class ScheduledQueue<TValue>
+    {
+        readonly PriorityQueue<DateTime, TValue> _data = new PriorityQueue<DateTime, TValue>();
+        readonly object _monitor = new object();
+        // If not null, there may be an active Next() call that is waiting for the next value.
+        Task _next = null;
+
+        // Adds an element to the queue. It'll be ready for processing at the specified time.
+        public void Push(TValue value, DateTime when)
+        {
+            lock (_monitor)
+            {
+                _data.Push(when, value);
+                if (_next != null)
+                {
+                    _next.RunSynchronously();
+                    _next = null;
+                }
+            }
+        }
+
+        // Returns the next ready message when it becomes ready.
+        // There must be at most one active task produced by Next().
+        // In other words, it's illegal to call Next() before the task produced
+        // by the previous call to Next() finishes.
+        public async Task<ReadyMessage<TValue>> Next(CancellationToken cancel)
+        {
+            while (true)
+            {
+                // TimeSpan.FromMilliseconds(-1) is infinity for Task.Delay().
+                TimeSpan delay = TimeSpan.FromMilliseconds(-1);
+                Task next = null;
+                lock (_monitor)
+                {
+                    if (_data.Any())
+                    {
+                        DateTime now = DateTime.UtcNow;
+                        if (_data.Front().Key <= now)
+                        {
+                            // Note that we may leave _next in non-null state. That's OK.
+                            return new ReadyMessage<TValue>()
+                            {
+                                Message = _data.Pop().Value,
+                                Last = !_data.Any() || _data.Front().Key > now
+                            };
+                        }
+                        delay = _data.Front().Key - now;
+                        // Task.Delay() can't handle values above TimeSpan.FromMilliseconds(Int32.MaxValue).
+                        if (delay > TimeSpan.FromMilliseconds(Int32.MaxValue))
+                        {
+                            delay = TimeSpan.FromMilliseconds(Int32.MaxValue);
+                        }
+                    }
+                    _next = new Task(delegate { }, cancel);
+                    next = _next;
+                }
+                // Task.WhenAny() returns Task<Task>, hence the double await.
+                await await Task.WhenAny(Task.Delay(delay, cancel), next);
+            }
+        }
+    }
+}
---
+++ ExchangeApi/Infra/Scheduler.cs
@@ -0,0 +1,69 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExchangeApi
+{
+    // Scheduler runs all actions asynchronously and serially. Different actions may
+    // run in different threads but they won't run concurrently.
+    // Actions can be scheduled in the future. The execution order is what you
+    // would expect: stable sorting by the scheduled time.
+    class Scheduler : IDisposable
+    {
+        private static readonly Logger _log = LogManager.GetCurrentClassLogger();
+
+        readonly ScheduledQueue<Action<bool>> _actions = new ScheduledQueue<Action<bool>>();
+        readonly CancellationTokenSource _dispose = new CancellationTokenSource();
+        readonly Task _loop;
+
+        // Launches a background task. Call Dispose() to stop it.
+        public Scheduler()
+        {
+            _loop = Task.Run(ActionLoop);
+        }
+
+        // Schedules the specified action to run at the specified time.
+        // The argument of the action is true if and only if there are no ready
+        // actions scheduled after it. In other words, it indicates the last ready
+        // action.
+        public void Schedule(Action<bool> action, DateTime when)
+        {
+            _actions.Push(action, when);
+        }
+
+        // Schedules the specified action to run ASAP.
+        public void Schedule(Action<bool> action)
+        {
+            Schedule(action, DateTime.UtcNow);
+        }
+
+        // Blocks until the background thread is stopped.
+        public void Dispose()
+        {
+            _log.Info("Disposing of ExchangeApi.Scheduler");
+            _dispose.Cancel();
+            try { _loop.Wait(); } catch { }
+            _log.Info("ExchangeApi.Scheduler successfully disposed of");
+        }
+
+        async Task ActionLoop()
+        {
+            while (true)
+            {
+                ReadyMessage<Action<bool>> action = await _actions.Next(_dispose.Token);
+                try
+                {
+                    action.Message.Invoke(action.Last);
+                }
+                catch (Exception e)
+                {
+                    _log.Warn(e, "Ignoring exception from the user action");
+                }
+            }
+        }
+    }
+}
