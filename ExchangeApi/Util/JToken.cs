21baa18aadc42fbbb45b569437e0ce9d0046f5b1
romkatv <roman.perepelitsa@gmail.com>
2016-03-05 12:10:26 +0100

    Support decimals serialized in scientific notation.

---
+++ ExchangeApi/Util/JToken.cs
@@ -0,0 +1,32 @@
+﻿using Conditions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeApi.Util
+{
+    public static class JToken
+    {
+        static readonly char[] E = new char[] { 'e', 'E' };
+
+        // Unlike regular decimal.Parse(), this one supports scientific notation.
+        //
+        // "-12.34" => -12.34
+        // "-12.34e-1" => -1.234
+        public static decimal AsDecimal(this Newtonsoft.Json.Linq.JToken token)
+        {
+            string s = (string)token;
+            Condition.Requires(s, "s").IsNotNull();
+            int pos = s.IndexOfAny(E);
+            if (pos == -1) return decimal.Parse(s);
+            decimal res = decimal.Parse(s.Substring(0, pos));
+            int e = int.Parse(s.Substring(pos + 1));
+            decimal mul = e > 0 ? 10m : 0.1m;
+            for (int i = 0; i != Math.Abs(e); ++i)
+                res *= mul;
+            return res;
+        }
+    }
+}
