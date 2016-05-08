4c80c7fcae83aafa280b35b3e3bc143ffddbb6aa
romkatv <roman.perepelitsa@gmail.com>
2016-05-08 13:11:33 +0200

    Update all nuget packages and remove the no longer needed workaround in Printable.

--- ExchangeApi/Util/Printable.cs
+++ ExchangeApi/Util/Printable.cs
@@ -1,8 +1,7 @@
-﻿using StatePrinter;
-using StatePrinter.Configurations;
-using StatePrinter.FieldHarvesters;
-using StatePrinter.Introspection;
-using StatePrinter.OutputFormatters;
+﻿using StatePrinting;
+using StatePrinting.FieldHarvesters;
+using StatePrinting.Introspection;
+using StatePrinting.OutputFormatters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,7 +15,6 @@ namespace ExchangeApi.Util
         public string Print(List<Token> tokens)
         {
             tokens = new UnusedReferencesTokenFilter().FilterUnusedReferences(tokens);
-            tokens = UnfuckupSimpleDicts(tokens);
             var output = new StringBuilder();
             for (int i = 0; i != tokens.Count; ++i)
             {
@@ -26,34 +24,6 @@ namespace ExchangeApi.Util
                     WriteToken(tokens[i], tokens[i + 1], output);
             }
             return output.ToString();
-        }
-
-        // Stateprinter has special logic to handle dictinaries with simple key types.
-        // It forces you to print such dictionaries as dict[key] = value, dict[key] = value, etc.
-        // We want it to look like dict = ([key] = value, [key] = value), just like lists.
-        public List<Token> UnfuckupSimpleDicts(List<Token> tokens)
-        {
-            var res = new List<Token>();
-            for (int i = 0; i != tokens.Count; ++i)
-            {
-                if (tokens[i].Tokenkind == TokenType.StartEnumeration &&
-                    i != 0 && i != tokens.Count - 1 &&
-                    tokens[i - 1].Tokenkind != TokenType.FieldnameWithTypeAndReference)
-                {
-                    if (tokens[i + 1].Tokenkind == TokenType.EndEnumeration)
-                    {
-                        ++i;
-                        continue;
-                    }
-                    Field next = tokens[i + 1].Field;
-                    if (next != null && next.SimpleKeyInArrayOrDictionary != null && next.Name != null)
-                    {
-                        res.Add(new Token(TokenType.FieldnameWithTypeAndReference, new Field(next.Name)));
-                    }
-                }
-                res.Add(tokens[i]);
-            }
-            return res;
         }
 
         void WriteToken(Token token, Token next, StringBuilder output)
@@ -69,10 +39,12 @@ namespace ExchangeApi.Util
                     MaybeWriteComma(next, output);
                     break;
 
-                case TokenType.StartEnumeration:
+                case TokenType.StartList:
+                case TokenType.StartDict:
                     output.Append("(");
                     break;
-                case TokenType.EndEnumeration:
+                case TokenType.EndList:
+                case TokenType.EndDict:
                     output.Append(")");
                     MaybeWriteComma(next, output);
                     break;
@@ -106,13 +78,10 @@ namespace ExchangeApi.Util
         {
             if (field == null)
                 return;
-            if (field.SimpleKeyInArrayOrDictionary != null)
-                output.AppendFormat("[{0}]", field.SimpleKeyInArrayOrDictionary);
+            if (field.Key != null)
+                output.AppendFormat("[{0}] = ", field.Key);
             else if (field.Name != null)
-                output.Append(field.Name);
-            else
-                return;
-            output.Append(" = ");
+                output.AppendFormat("{0} = ", field.Name);
         }
 
         void MaybeWriteComma(Token next, StringBuilder output)
