d493725f74b2243930e2178a8d1ef571905f7c56
romkatv <roman.perepelitsa@gmail.com>
2016-05-08 18:04:08 +0200

    Coinbase: Split order book orders into bids and asks.

--- ExchangeApi/Coinbase/Messages.cs
+++ ExchangeApi/Coinbase/Messages.cs
@@ -45,26 +45,24 @@ namespace ExchangeApi.Coinbase
     public enum DoneReason
     {
         Filled,
-        Cancelled,
-    }
-
-    public class Amount : Util.Printable<Amount>
-    {
-        public Side Side { get; set; }
-        public decimal Price { get; set; }
-        public decimal Quantity { get; set; }
+        Canceled,
     }
 
     public class Order : Util.Printable<Order>
     {
         public string Id { get; set; }
-        public Amount Amount { get; set; }
+        public decimal Price { get; set; }
+        public decimal Quantity { get; set; }
     }
 
     public class OrderBook : Util.Printable<OrderBook>
     {
+        // Server time. Coinbase doesn't give us server time together with the order book,
+        // so we it with separate request BEFORE requesting the order book.
+        public DateTime Time;
         public long Sequence { get; set; }
-        public List<Order> Orders { get; set; }
+        public List<Order> Bids { get; set; }
+        public List<Order> Asks { get; set; }
     }
 
     public class SubscribeRequest : Util.Printable<SubscribeRequest>, IMessageOut
