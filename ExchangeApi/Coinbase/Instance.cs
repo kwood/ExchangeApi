863228a05807ddba3f02f14bbaf170219b402d55
romkatv <roman.perepelitsa@gmail.com>
2016-05-08 13:00:23 +0200

    Coinbase: Make interface of Instance consistent with OkCoin's.

--- ExchangeApi/Coinbase/Instance.cs
+++ ExchangeApi/Coinbase/Instance.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Conditions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,20 +7,28 @@ using System.Threading.Tasks;
 
 namespace ExchangeApi.Coinbase
 {
-    public static class Instance
+    public class Instance
     {
-        public static class Prod
+        readonly string _websocket;
+        readonly string _rest;
+
+        public Instance(string websocket, string rest)
         {
-            // Web interface: https://exchange.coinbase.com/
-            public static readonly string REST = "https://api.exchange.coinbase.com";
-            public static readonly string WebSocket = "wss://ws-feed.exchange.coinbase.com";
+            Condition.Requires(websocket, "websocket").IsNotNullOrEmpty();
+            Condition.Requires(rest, "rest").IsNotNullOrEmpty();
+            _websocket = websocket;
+            _rest = rest;
         }
 
-        public static class Sandbox
-        {
-            // Web interface: https://public.sandbox.exchange.coinbase.com/.
-            public static readonly string REST = "https://api-public.sandbox.exchange.coinbase.com";
-            public static readonly string WebSocket = "wss://ws-feed-public.sandbox.exchange.coinbase.com";
-        }
+        public string WebSocket { get { return _websocket; } }
+        public string REST { get { return _rest; } }
+
+        // Web interface: https://exchange.coinbase.com/.
+        public static Instance Prod = new Instance(
+            "wss://ws-feed.exchange.coinbase.com", "https://api.exchange.coinbase.com");
+
+        // Web interface: https://public.sandbox.exchange.coinbase.com/.
+        public static Instance Sandbox = new Instance(
+            "wss://ws-feed-public.sandbox.exchange.coinbase.com", "https://api-public.sandbox.exchange.coinbase.com");
     }
 }
